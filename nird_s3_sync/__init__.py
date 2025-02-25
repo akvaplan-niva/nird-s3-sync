95c5d7dc6f98719a636e1b2ef96fe571ed370449
Mathieu Tachon <mta@akvaplan.niva.no>
2025-02-25 11:57:32 +0100

    Add short docstring for package description

---
+++ nird_s3_sync/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+NIRD S3 Sync
+============
+
+The *The NIRD S3 Sync* packages provides a way to fetch data
+stored on `NIRD S3 (Sigma2) <https://documentation.sigma2.no/files_storage/nird/s3.html>`_,
+and copy/synchronize it with a specified location on another file-system.
+"""
