7d48e6db9e36147155cfb7122e153dc708c4d32f
Mathieu Tachon <mta@akvaplan.niva.no>
2025-02-25 14:06:58 +0100

    Fix line too long

--- nird_s3_sync/__init__.py
+++ nird_s3_sync/__init__.py
@@ -5,6 +5,7 @@ NIRD S3 Sync
 ============
 
 The *The NIRD S3 Sync* packages provides a way to fetch data
-stored on `NIRD S3 (Sigma2) <https://documentation.sigma2.no/files_storage/nird/s3.html>`_,
+stored on
+`NIRD S3 (Sigma2) <https://documentation.sigma2.no/files_storage/nird/s3.html>`_,
 and copy/synchronize it with a specified location on another file-system.
 """
