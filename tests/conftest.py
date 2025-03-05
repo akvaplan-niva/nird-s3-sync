7a601443ccc9611f9f62ebf4f9af3c5e7b575402
Mathieu Tachon <mta@akvaplan.niva.no>
2025-03-05 13:38:15 +0100

    test(io): Add test_fs2fs_copy

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,3 +18,19 @@ def f_tmp_file():
 @pytest.fixture
 def f_tmp_local_fs():
     return fsspec.filesystem("file")
+
+
+@pytest.fixture
+def f_minio_fs_file():
+    fs = fsspec.filesystem(
+        "s3",
+        key=os.getenv("MINIO_ROOT_USER"),
+        secret=os.getenv("MINIO_ROOT_PASSWORD"),
+        endpoint_url=f"http://localhost:{os.getenv('MINIO_PORT')}",
+    )
+    bucket_name = os.getenv("MINIO_BUCKET_NAME")
+    fs.mkdir(bucket_name)
+    test_filename = "test_file.txt"
+    filepath = os.path.join(bucket_name, test_filename)
+    yield fs, filepath
+    fs.rm(bucket_name, recursive=True)
--- tests/test_nird_s3_sync/test_io.py
+++ tests/test_nird_s3_sync/test_io.py
@@ -2,11 +2,12 @@
 
 
 import hashlib
+from pathlib import Path
 
 import hypothesis.strategies as st
 from hypothesis import HealthCheck, given, settings
 
-from nird_s3_sync.io import get_file_sha256_checksum
+from nird_s3_sync.io import fs2fs_copy, get_file_sha256_checksum
 
 
 @settings(
@@ -21,3 +22,23 @@ def test_get_file_sha256_checksum(f_tmp_file, f_tmp_local_fs, bytes_):
     assert sha256_hash.hexdigest() == get_file_sha256_checksum(
         f_tmp_local_fs, f_tmp_file
     )
+
+
+def test_fs2fs_copy(tmp_path, f_minio_fs_file, f_tmp_local_fs):
+    minio_fs, minio_filepath = f_minio_fs_file
+    # Write random content in file
+    with minio_fs.open(
+        minio_filepath,
+        mode="w",
+        encoding="utf-8",
+    ) as f:
+        f.write("Some content")
+    output_path = tmp_path / Path(minio_filepath).name
+    assert not output_path.exists()
+    _ = fs2fs_copy(
+        minio_fs,
+        minio_filepath,
+        f_tmp_local_fs,
+        output_path.as_posix(),
+    )
+    assert output_path.exists()
