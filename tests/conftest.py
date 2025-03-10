6cf0ef814070ad3c8d30801d56cc5dc8c407ab28
Mathieu Tachon <mta@akvaplan.niva.no>
2025-03-10 14:49:23 +0100

    Test printing user for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,6 +29,7 @@ def f_minio_fs_file():
         endpoint_url=f"http://127.0.0.1:{os.getenv('MINIO_PORT')}",
     )
     bucket_name = os.getenv("MINIO_BUCKET_NAME")
+    print(f"MINIO_ROOT_USER: {os.getenv('MINIO_ROOT_USER')}")
     fs.mkdir(bucket_name)
     test_filename = "test_file.txt"
     filepath = os.path.join(bucket_name, test_filename)
