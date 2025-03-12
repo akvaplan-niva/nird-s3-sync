f092455f0b89ac7e2382b3ab3b672b4c9a03671d
Mathieu Tachon <mta@akvaplan.niva.no>
2025-03-12 13:11:24 +0100

    test(utils): Add test_get_fs

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,9 +29,22 @@ def f_minio_fs_file():
         endpoint_url=f"http://127.0.0.1:{os.getenv('MINIO_PORT')}",
     )
     bucket_name = os.getenv("MINIO_BUCKET_NAME")
-    print(f"MINIO_ROOT_USER: {os.getenv('MINIO_ROOT_USER')}")
     fs.mkdir(bucket_name)
     test_filename = "test_file.txt"
     filepath = os.path.join(bucket_name, test_filename)
     yield fs, filepath
+    fs.rm(bucket_name, recursive=True)
+
+
+@pytest.fixture
+def f_minio_fs_bucket():
+    fs = fsspec.filesystem(
+        "s3",
+        key=os.getenv("MINIO_ROOT_USER"),
+        secret=os.getenv("MINIO_ROOT_PASSWORD"),
+        endpoint_url=f"http://127.0.0.1:{os.getenv('MINIO_PORT')}",
+    )
+    bucket_name = os.getenv("MINIO_BUCKET_NAME")
+    fs.mkdir(bucket_name)
+    yield fs, bucket_name
     fs.rm(bucket_name, recursive=True)
--- tests/test_nird_s3_sync/test_utils.py
+++ tests/test_nird_s3_sync/test_utils.py
@@ -2,10 +2,13 @@
 
 
 import json
+import os
+from unittest.mock import patch
 
 import pytest
+import s3fs
 
-from nird_s3_sync.utils import get_credentials
+from nird_s3_sync import utils
 
 
 @pytest.fixture
@@ -21,7 +24,7 @@ def mock_missing_env_vars(monkeypatch):
 
 
 def test_get_credentials(mock_env_vars):
-    credentials = get_credentials()
+    credentials = utils.get_credentials()
     expected_credentials = {
         "key": "SOME_ACCESS_KEY",
         "secret": "SOME_SECRET_KEY",
@@ -33,4 +36,29 @@ def test_get_credentials(mock_env_vars):
 
 def test_get_missing_credentials(mock_missing_env_vars):
     with pytest.raises(RuntimeError):
-        _ = get_credentials()
+        _ = utils.get_credentials()
+
+
+def test_get_fs(monkeypatch, f_minio_fs_bucket):
+    monkeypatch.setenv(
+        "ACCESS_KEY_NIRD_S3",
+        os.getenv("MINIO_ROOT_USER"),
+        prepend=False,
+    )
+    monkeypatch.setenv(
+        "SECRET_KEY_NIRD_S3",
+        os.getenv("MINIO_ROOT_PASSWORD"),
+        prepend=False,
+    )
+    _, bucket_name = f_minio_fs_bucket
+    with patch.object(
+        utils,
+        "NIRD_S3_ENDPOINT_URL",
+        f"http://127.0.0.1:{os.getenv('MINIO_PORT')}",
+    ):
+        fs = utils.get_fs(bucket_name)
+        assert isinstance(fs, s3fs.core.S3FileSystem)
+        assert hasattr(fs, "bucket_name")
+        assert fs.bucket_name == bucket_name
+        with pytest.raises(FileNotFoundError):
+            _ = utils.get_fs("not_a_bucket")
